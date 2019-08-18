ea5e09984420f862516e6e8e4b511c18f5d04c4d
Gabriele Proietti Mattia <gabry.gabry@hotmail.it>
2019-08-18 14:11:11 +0200

    Init with test

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { HTTP, HTTPResponse } from '@ionic-native/http/ngx';
+
+const BASE_URL = 'https://flyfantastic.pythonanywhere.com/';
 
 @Component({
   selector: 'app-home',
@@ -7,6 +10,50 @@ import { Component } from '@angular/core';
 })
 export class HomePage {
 
-  constructor() {}
+  private mHttp: HTTP;
+  loading = false;
+  result = '';
+  resultTitle = 'Results';
+
+  constructor() {
+    this.mHttp = new HTTP();
+  }
+
+  downloadCookie() {
+    this.loading = true;
+    this.resultTitle = 'Download Cookie';
+    this.mHttp.get(`${BASE_URL}/set-cookie`, null, null).then(res => {
+      this.result = res.data;
+      this.loading = false;
+    }).catch(err => {
+      this.result = JSON.stringify(err);
+      this.loading = false;
+    });
+  }
+
+  printCookie() {
+    this.resultTitle = 'Print Cookie';
+    this.result = this.mHttp.getCookieString(`${BASE_URL}`);
+  }
+
+  clearCookies() {
+    this.resultTitle = 'Clear Cookies';
+    this.mHttp.clearCookies();
+    this.result = 'Cleared!';
+  }
+
+  checkCookie() {
+    this.loading = true;
+    this.resultTitle = 'Check Cookie';
+    this.mHttp.get(`${BASE_URL}`, null, null).then(res => {
+      this.result = res.data;
+      this.loading = false;
+    }).catch(err => {
+      this.result = JSON.stringify(err);
+      this.loading = false;
+    });
+  }
 
 }
+
+
